56ce68d616bf7f3e3559d56cbcf8237dd47f3de9
YuryShageev <schageev.yur@yandex.ru>
2022-11-15 11:12:25 +0300

    Продолжение первой домашки

--- src/Automobile.java
+++ src/Automobile.java
@@ -1,2 +1,42 @@
 public class Automobile {
+
+    String brand;
+    String model;
+    float engineVolume;
+    String color;
+    int year;
+    String country;
+
+    Automobile(String brand, String model, int year, String country, String color, float engineVolume) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.country = country;
+        this.color = color;
+        this.engineVolume = engineVolume;
+    }
+
+    Automobile() {
+        brand = "Гнилая";
+        model = "тачанка";
+        year = 1960;
+        country = "Черти где";
+        color = "Такой же как красный, только синий";
+        engineVolume = 1.1f;
+    }
+
+    void CarParameters(String brand, String model, int year, String country, String color, float engineVolume) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.country = country;
+        this.color = color;
+        this.engineVolume = engineVolume;
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + ", " + year + " года выпуска, сборка в стране " + country + ", " + color + " цвета, объем двигателя - " + engineVolume + "л.";
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Automobile automobile = new Automobile();
+        System.out.println(automobile);
+        Automobile automobile1 = new Automobile();
+        automobile1.brand = "Lada";
+        automobile1.model = "Granta";
+        automobile1.year = 2015;
+        automobile1.country = "Russia";
+        automobile1.color = "жёлтого";
+        automobile1.engineVolume = 1.7f;
+        System.out.println(automobile1);
+
+        Automobile automobile2 = new Automobile("Audi", "A8 50 L TDI quattro", 2020, "Германия", " чёрного", 3.0f);
+        System.out.println(automobile2);
+        Automobile automobile3 = new Automobile("BMW", "Z8", 2021, "Германия", " чёрного", 3.0f);
+        System.out.println(automobile3);
+        Automobile automobile4 = new Automobile("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "красного", 2.4f);
+        System.out.println(automobile4);
+        Automobile automobile5 = new Automobile("Hyundai", "Avante", 2016, "Южная Корея", "оранжевого", 1.6f);
+        System.out.println(automobile5);
+    }
 }
